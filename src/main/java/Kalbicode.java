e70f5f1da464abe78e26780f690356f37b9f7353
KalbinurK <kkalbinur@yahoo.com>
2020-11-01 15:54:03 -0500

    this is the first commit

---
+++ src/main/java/Kalbicode.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Kalbicode {
+
+    public static void main(String[] args) {
+        System.out.println("hello everyone");
+    }
+}
